de5e941cdb3a83c4c329f9cd8b88f1d0525294e3
Airin100 <airinakter114@gmail.com>
2025-05-20 20:11:58 +0600

    Add requirments for Render

--- app.py
+++ app.py
@@ -169,5 +169,5 @@ def reject_user(data):
                                    room = room)
                      
                      if __name__ == '__main__':
-                        port = int(os.environ.get('PORT',5000))
+                        port = int(os.environ.get('PORT', 5000))
                         socketio.run(app, host = '0.0.0.0', port = port)
